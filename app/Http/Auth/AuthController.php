daa66cdd1e455c3df82b43f6b0597287623858c9
Gimhan Rodrigo <gimhanrg@gmail.com>
2021-06-06 00:44:12 +0530

    routes

---
+++ app/Http/Auth/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Http\Controllers\Auth;
+  
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function _construct(){
+        $this->middleware(['guest']);
+    }
+
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+    
+    public function loginValidate(Request $request)
+    {  
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+    
+        ]);
+        
+        if(!auth()->attempt($request->only('email','password'), $request->remember)){
+            return back()->with('status','The login details are incorrect');
+        }else{
+            if(auth->user()->level=='Admin'){
+
+            }else if(auth->user()->level=='Officer'){
+
+            }
+            else{
+                return redirect()->route('dashboard');
+            
+            }
+           
+        
+        }
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function store(Request $request)
+    {  
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'phone' => 'required',
+    
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+            'level' => 'Customer',
+        ]);
+           
+        return redirect()->route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'loginValidate'])->name('login.post');
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('registration', [AuthController::class, 'store'])->name('register.post');
 Route::apiResource('vehicle_advertisements','VehicleAdvertisementsController');
+
+
 
